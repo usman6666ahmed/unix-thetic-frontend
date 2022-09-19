c4532888ed26101bf8f7ca2a082358849b10d049
usman6666ahmed <usman.imtiaz@devsinc.com>
2022-09-19 13:07:42 +0500

    Material UI
    * Removed Tailwind
    * Installed MUI with ng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersListComponent } from './components/users-list/users-list.component';
 import { LoginComponent } from './pages/login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HomeComponent } from './pages/home/home.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './modules/material.module';
 
 @NgModule({
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MaterialModule,
   ],
-  declarations: [AppComponent, UsersListComponent, LoginComponent, HomeComponent],
+  declarations: [
+    AppComponent,
+    UsersListComponent,
+    LoginComponent,
+    HomeComponent,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,22 +1,38 @@
 import { User } from './../../../types/resources';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
   users: User[] = [];
+  apiUrl = 'https://localhost:3001';
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private httpClient: HttpClient
+  ) {}
 
   loginForm = this.formBuilder.group({
-    username: '',
+    username: 'user',
+    password: '123123',
   });
 
   async onSubmit() {
-    console.log(this.loginForm.value);
+    const url = `${this.apiUrl}/auth/register`;
+    const req = this.httpClient.post(url, this.loginForm.value);
+    req.subscribe(
+      (data) => {
+        console.log(data);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 
   ngOnInit(): void {}
